b39fd3fd001963ff90d2e4f63072c37ec3e7c904
Eimar <eimar.ramossacari@gmail.com>
2024-11-30 06:50:31 +0100

    Actualización de dependencias y configuración de Maui
    Se ha actualizado el archivo `ApplicationLayer.csproj` para incluir una referencia al paquete `Microsoft.Extensions.DependencyInjection.Abstractions` versión `9.0.0`.
    
    Se ha actualizado el archivo `Infrastructure.csproj` para incluir referencias a los paquetes `AutoMapper` versión `13.0.1`, `Microsoft.EntityFrameworkCore.Sqlite` versión `9.0.0` y `Microsoft.Extensions.DependencyInjection.Abstractions` versión `9.0.0`.
    
    En el archivo `MauiProgram.cs`, se ha añadido el uso de `CommunityToolkit.Maui` y se han creado métodos de extensión para registrar servicios, modelos de vista y vistas. Además, se ha ajustado la construcción de la aplicación para utilizar estos métodos de registro.
    
    En el archivo `UI.csproj`, se ha ajustado la configuración del `MauiSplashScreen` para incluir un espacio adicional al final de la línea. También se han añadido referencias a los paquetes `CommunityToolkit.Maui` versión `9.1.1` y `CommunityToolkit.Mvvm` versión `8.3.2`.

--- src/UI/MauiProgram.cs
+++ src/UI/MauiProgram.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using CommunityToolkit.Maui;
+using Microsoft.Extensions.Logging;
 
 namespace UI
 {
@@ -9,17 +10,43 @@ namespace UI
             var builder = MauiApp.CreateBuilder();
             builder
                 .UseMauiApp<App>()
+                .UseMauiCommunityToolkit()
                 .ConfigureFonts(fonts =>
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-                });
+                })
+                .RegisterServices()
+                .RegisterViewModels()
+                .RegisterViews(); ;
 
 #if DEBUG
     		builder.Logging.AddDebug();
 #endif
 
-            return builder.Build();
+            var app = builder.Build();
+
+            return app;
+        }
+
+        public static MauiAppBuilder RegisterServices(this MauiAppBuilder mauiAppBuilder)
+        {
+ 
+
+            return mauiAppBuilder;
+        }
+
+        public static MauiAppBuilder RegisterViewModels(this MauiAppBuilder mauiAppBuilder)
+        {
+
+
+            return mauiAppBuilder;
+        }
+
+        public static MauiAppBuilder RegisterViews(this MauiAppBuilder mauiAppBuilder)
+        {
+
+            return mauiAppBuilder;
         }
     }
 }
