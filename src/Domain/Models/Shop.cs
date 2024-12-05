b4ff5b9e3e3798cd36a6fec0c21742b7fb66fa9c
Eimar <eimar.ramossacari@gmail.com>
2024-12-05 11:30:42 +0100

    Añadir propiedades `ImageSource` y `CategoriesString` a `Shop`
    Se han añadido dos nuevas propiedades a la clase `Shop`: `ImageSource` y `CategoriesString`, ambas inicializadas con una cadena vacía.

--- src/Domain/Models/Shop.cs
+++ src/Domain/Models/Shop.cs
@@ -5,5 +5,7 @@
         public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
         public double Rating { get; set; }
+        public string ImageSource { get; set; } = string.Empty;
+        public string CategoriesString { get; set; } = string.Empty;
     }
 }
