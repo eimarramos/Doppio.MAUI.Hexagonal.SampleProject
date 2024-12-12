af2fe06370e40015c5408b654468209368f8bda5
Eimar <eimar.ramossacari@gmail.com>
2024-12-12 21:01:57 +0100

    Añadir ShopDetailsViewModel y navegación a home
    Se han añadido las directivas `using` para `CommunityToolkit.Maui.Views`,
    `CommunityToolkit.Mvvm.ComponentModel` y `CommunityToolkit.Mvvm.Input`.
    Se ha añadido la declaración del espacio de nombres `UI.ViewModels`.
    Se ha creado la clase `ShopDetailsViewModel` que hereda de `BaseViewModel`
    e implementa la interfaz `IQueryAttributable`.
    Se ha añadido una propiedad observable `Shop` de tipo `Shop?`.
    Se ha añadido un constructor vacío para `ShopDetailsViewModel`.
    Se ha añadido el comando `GoHome` que ejecuta una tarea asíncrona para
    navegar a la página "home" y maneja posibles excepciones.
    Se ha implementado el método `ApplyQueryAttributes` que asigna un valor
    a la propiedad `Shop` a partir de un diccionario de consulta.

---
+++ src/UI/ViewModels/ShopDetailsViewModel.cs
@@ -0,0 +1,42 @@
+﻿using CommunityToolkit.Maui.Views;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Domain.Models;
+
+namespace UI.ViewModels
+{
+    public partial class ShopDetailsViewModel : BaseViewModel, IQueryAttributable
+    {
+        [ObservableProperty]
+        private Shop? shop;
+        public ShopDetailsViewModel()
+        {}
+
+        [RelayCommand]
+        private async Task GoHome()
+        {
+            await Task.Run(async () =>
+            {
+                try
+                {
+                    IsBusy = true;
+
+                    await Shell.Current.GoToAsync("///home");
+                }
+                catch (Exception e)
+                {
+                    Console.Error.WriteLine(e);
+                }
+                finally
+                {
+                    IsBusy = false;
+                }
+            });
+        }
+
+        public void ApplyQueryAttributes(IDictionary<string, object> query)
+        {
+            Shop = query["Shop"] as Shop;
+        }
+    }
+}
