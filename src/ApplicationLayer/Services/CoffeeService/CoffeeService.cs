06ecfb1baa8cd986fecd67ffd02b046e4761ce54
Eimar <eimar.ramossacari@gmail.com>
2024-12-24 22:34:44 +0100

    Añadir CoffeeService con métodos para obtener cafés
    Se ha añadido la clase `CoffeeService` en `CoffeeService.cs`.
    Se han importado los espacios de nombres `Domain.Models` y `Domain.Repositories`.
    La clase incluye un campo privado `_coffeeRepository` de tipo `ICoffeeRepository`.
    El constructor de `CoffeeService` inicializa `_coffeeRepository`.
    Se han añadido dos métodos asíncronos:
    - `GetAllByShopId(int shopId)`: Devuelve una lista de `Coffee` filtrada por ID de tienda.
    - `GetTopThreeByShopId(int shopId)`: Devuelve las tres mejores instancias de `Coffee` filtradas por ID de tienda.

---
+++ src/ApplicationLayer/Services/CoffeeService/CoffeeService.cs
@@ -0,0 +1,25 @@
+﻿using Domain.Models;
+using Domain.Repositories;
+
+namespace ApplicationLayer.Services.CoffeeService
+{
+    public class CoffeeService
+    {
+        private readonly ICoffeeRepository _coffeeRepository;
+
+        public CoffeeService(ICoffeeRepository coffeeRepository)
+        {
+            _coffeeRepository = coffeeRepository;
+        }
+
+        public async Task<List<Coffee>> GetAllByShopId(int shopId)
+        {
+            return await _coffeeRepository.GetAllByShopId(shopId);
+        }
+
+        public async Task<List<Coffee>> GetTopThreeByShopId(int shopId)
+        {
+            return await _coffeeRepository.GetTopThreeByShopId(shopId);
+        }
+    }
+}
