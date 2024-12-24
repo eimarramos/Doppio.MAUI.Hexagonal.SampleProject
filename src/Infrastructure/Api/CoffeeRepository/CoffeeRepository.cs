79295bfd81bce3a7b5409cf246f4497bb1edd529
Eimar <eimar.ramossacari@gmail.com>
2024-12-24 22:35:37 +0100

    Implementación de CoffeeRepository con métodos de consulta
    Se añadieron varias declaraciones `using` para importar los espacios de nombres necesarios, incluyendo `AutoMapper`, `Domain.Models`, `Domain.Repositories`, `Infrastructure.Entities`, `Infrastructure.Persistence` y `Microsoft.EntityFrameworkCore`.
    
    Se creó un nuevo espacio de nombres `Infrastructure.Api.CoffeeRepository`.
    
    Se añadió una nueva clase `CoffeeRepository` que implementa la interfaz `ICoffeeRepository`.
    
    Se añadieron dos campos privados, `_context` y `_mapper`, que se inicializan a través del constructor de la clase `CoffeeRepository`.
    
    Se añadió el método `GetAllByShopId` que obtiene todas las instancias de `Coffee` asociadas a un `shopId` específico. Este método utiliza Entity Framework Core para incluir las entidades `Coffees` relacionadas y luego mapea las entidades `CoffeeEntity` a modelos de dominio `Coffee` utilizando AutoMapper.
    
    Se añadió el método `GetTopThreeByShopId` que obtiene las tres primeras instancias de `Coffee` asociadas a un `shopId` específico. Similar al método anterior, utiliza Entity Framework Core y AutoMapper para realizar la operación.

---
+++ src/Infrastructure/Api/CoffeeRepository/CoffeeRepository.cs
@@ -0,0 +1,47 @@
+﻿using AutoMapper;
+using Domain.Models;
+using Domain.Repositories;
+using Infrastructure.Entities;
+using Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Api.CoffeeRepository
+{
+    public class CoffeeRepository : ICoffeeRepository
+    {
+        private readonly DatabaseContext _context;
+        private readonly IMapper _mapper;
+        public CoffeeRepository(DatabaseContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+        public async Task<List<Coffee>> GetAllByShopId(int shopId)
+        {
+            ShopEntity? shop = await _context.Shops
+                                             .Include(s => s.Coffees)
+                                             .FirstOrDefaultAsync(c => c.Id == shopId);
+
+            if (shop == null) return new List<Coffee>();
+
+            List<CoffeeEntity> coffees = shop.Coffees.ToList();
+
+            return _mapper.Map<List<Coffee>>(coffees);
+        }
+
+        public async Task<List<Coffee>> GetTopThreeByShopId(int shopId)
+        {
+            var shop = await _context.Shops
+                                     .Include(s => s.Coffees)
+                                     .FirstOrDefaultAsync(c => c.Id == shopId);
+
+            if (shop == null) return new List<Coffee>();
+
+            List<CoffeeEntity> topThreeCoffees = shop.Coffees
+                                                     .Take(3)
+                                                     .ToList();
+
+            return _mapper.Map<List<Coffee>>(topThreeCoffees);
+        }
+    }
+}
