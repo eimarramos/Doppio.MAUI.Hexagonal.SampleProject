f3ee4750dfa4c927ae267f4d4e98bd69e98f8ecc
Eimar <eimar.ramossacari@gmail.com>
2024-12-24 22:35:12 +0100

    Añadir clase Coffee en Domain.Models
    Se ha añadido un nuevo archivo `Coffee.cs` en el espacio de nombres `Domain.Models`. En este archivo, se ha definido una nueva clase pública llamada `Coffee` con las siguientes propiedades:
    - `Id` de tipo `int`.
    - `Name` de tipo `string` con un valor predeterminado de cadena vacía.
    - `Description` de tipo `string` con un valor predeterminado de cadena vacía.
    - `ImageSource` de tipo `string` con un valor predeterminado de cadena vacía.
    - `Price` de tipo `decimal`.
    
    Esta clase representa las características de un café.

---
+++ src/Domain/Models/Coffee.cs
@@ -0,0 +1,12 @@
+﻿namespace Domain.Models
+{
+    public class Coffee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string ImageSource { get; set; } = string.Empty;
+
+        public decimal Price { get; set; }
+    }
+}
