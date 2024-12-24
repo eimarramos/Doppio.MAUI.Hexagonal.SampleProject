3f65a9b2587c4ff51f8096128fbe14eea7647c1f
Eimar <eimar.ramossacari@gmail.com>
2024-12-24 22:35:21 +0100

    Añadir interfaz ICoffeeRepository con métodos asíncronos
    Se ha añadido una nueva interfaz `ICoffeeRepository` en el archivo
    `ICoffeeRepository.cs` dentro del espacio de nombres
    `Domain.Repositories`. La interfaz define dos métodos asíncronos:
    - `GetAllByShopId(int shopId)`: Devuelve una lista de objetos `Coffee`
      filtrados por el identificador de la tienda.
    - `GetTopThreeByShopId(int shopId)`: Devuelve una lista con los tres
      mejores objetos `Coffee` filtrados por el identificador de la tienda.

---
+++ src/Domain/Repositories/ICoffeeRepository.cs
@@ -0,0 +1,10 @@
+﻿using Domain.Models;
+
+namespace Domain.Repositories
+{
+    public interface ICoffeeRepository
+    {
+        Task<List<Coffee>> GetAllByShopId(int shopId);
+        Task<List<Coffee>> GetTopThreeByShopId(int shopId);
+    }
+}
