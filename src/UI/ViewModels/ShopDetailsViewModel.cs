cbe337d008ddd64feec379d37bee4e5b96ce45a8
Eimar <eimar.ramossacari@gmail.com>
2024-12-26 23:33:51 +0100

    Refactorización de ShopDetailsViewModel
    - Añadida referencia a CoffeeService en ShopDetailsViewModel.cs.
    - Eliminada anotación [QueryProperty(nameof(Shop), "Shop")].
    - Implementada interfaz IQueryAttributable en ShopDetailsViewModel.
    - Añadida propiedad observable shop inicializada con Shop.
    - Añadida colección observable coffees para objetos Coffee.
    - Añadido constructor con parámetro CoffeeService.
    - Añadido método ApplyQueryAttributes para manejar atributos de consulta.
    - Añadido método LoadDataAsync para carga de datos asíncrona.
    - Añadido método GetCoffeesAsync para obtener cafés del servicio.

--- src/UI/ViewModels/ShopDetailsViewModel.cs
+++ src/UI/ViewModels/ShopDetailsViewModel.cs
@@ -1,16 +1,60 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using ApplicationLayer.Services.CoffeeService;
+using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using Domain.Models;
+using System.Collections.ObjectModel;
 
 namespace UI.ViewModels
 {
-    [QueryProperty(nameof(Shop), "Shop")]
-    public partial class ShopDetailsViewModel : BaseViewModel
+    public partial class ShopDetailsViewModel : BaseViewModel, IQueryAttributable
     {
+        private readonly CoffeeService _coffeeService;
+
         [ObservableProperty]
-        private Shop? shop;
-        public ShopDetailsViewModel()
-        { }
+        private Shop shop = new Shop();
+        [ObservableProperty]
+        private ObservableCollection<Coffee> coffees = new ObservableCollection<Coffee>();
+
+        public ShopDetailsViewModel(CoffeeService coffeeService)
+        {
+            _coffeeService = coffeeService;       
+        }
+
+        public void ApplyQueryAttributes(IDictionary<string, object> query)
+        {
+            if (query.TryGetValue("Shop", out var shopObj) && shopObj is Shop shop)
+            {
+                Shop = shop;
+                LoadDataAsync();
+            }
+        }
+
+        public async void LoadDataAsync()
+        {
+            try
+            {
+                IsBusy = true;
+
+                await GetCoffeesAsync();
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine(ex);
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+        }
+
+        private async Task GetCoffeesAsync()
+        {
+            List<Coffee> coffees = await _coffeeService.GetTopThreeByShopId(Shop.Id);
+
+            if (coffees == null) return;
+
+            Coffees = new ObservableCollection<Coffee>(coffees);
+        }
 
         [RelayCommand]
         private async Task GoBack()
