4e394cea859d4d411526ae9e6c43cce3f02691cd
Eimar <eimar.ramossacari@gmail.com>
2025-01-07 23:53:53 +0100

    Añadir interfaz ICartRepository con métodos asíncronos
    Se ha añadido una nueva interfaz `ICartRepository` en el archivo `ICartRepository.cs` dentro del espacio de nombres `Domain.Repositories`. La interfaz define tres métodos asíncronos: `GetCoffesInCart(int userId)`, `AddCoffeeToCart(int userId, int coffeeId)` y `RemoveCoffeeFromCart(int userId, int coffeeId)`.

---
+++ src/Domain/Repositories/ICartRepository.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Models;
+
+namespace Domain.Repositories
+{
+    public interface ICartRepository
+    {
+        Task<List<Coffee>> GetCoffesInCart(int userId);
+        Task AddCoffeeToCart(int userId, int coffeeId);
+        Task RemoveCoffeeFromCart(int userId, int coffeeId);
+    }
+}
