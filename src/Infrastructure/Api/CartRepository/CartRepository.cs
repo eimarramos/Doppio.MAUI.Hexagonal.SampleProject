e3d3ed50ef60898e89fe0eda9140d6a16cb71761
Eimar <eimar.ramossacari@gmail.com>
2025-01-10 23:26:48 +0100

    Implementación inicial de CartRepository
    - Añadida importación del espacio de nombres `Domain.Repositories`.
    - Modificada declaración del espacio de nombres en `Infrastructure.Api.CartRepository`.
    - `CartRepository` ahora implementa `ICartRepository`.
    - Añadidos métodos `AddCoffeeToCart`, `GetCoffesInCart` y `RemoveCoffeeFromCart` con `NotImplementedException`.

--- src/Infrastructure/Api/CartRepository/CartRepository.cs
+++ src/Infrastructure/Api/CartRepository/CartRepository.cs
@@ -1,6 +1,23 @@
-﻿namespace Infrastructure.Api.CartRepository
+﻿using Domain.Models;
+using Domain.Repositories;
+
+namespace Infrastructure.Api.CartRepository
 {
-    public class CartRepository
+    public class CartRepository : ICartRepository
     {
+        public Task AddCoffeeToCart(int coffeeId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Coffee>> GetCoffesInCart()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RemoveCoffeeFromCart(int coffeeId)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
