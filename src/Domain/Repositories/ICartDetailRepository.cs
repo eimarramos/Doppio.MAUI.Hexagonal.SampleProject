ab70678258d8355040b1ab9d53185ed309ae6de7
Eimar <eimar.ramossacari@gmail.com>
2025-01-13 23:22:19 +0100

    Añadir interfaz ICartDetailRepository con métodos async
    Se ha introducido la interfaz `ICartDetailRepository` en el archivo `ICartDetailRepository.cs` dentro del espacio de nombres `Domain.Repositories`. Esta interfaz define tres métodos asíncronos: `GetCoffes()`, `AddCoffeeToCart(int coffeeId)` y `RemoveCoffeeFromCart(int coffeeId)`, para gestionar los detalles del carrito de compras relacionados con los objetos `Coffee`.

---
+++ src/Domain/Repositories/ICartDetailRepository.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Models;
+
+namespace Domain.Repositories
+{
+    public interface ICartDetailRepository
+    {
+        Task<List<Coffee>> GetCoffes();
+        Task AddCoffeeToCart(int coffeeId);
+        Task RemoveCoffeeFromCart(int coffeeId);
+    }
+}
