dc063eabd4f5dd561da88e15d5458fc89502a9ee
Eimar <eimar.ramossacari@gmail.com>
2025-01-13 23:22:40 +0100

    Refactorización de CartRepository
    Se han eliminado las importaciones de `Domain.Models` y `Domain.Repositories`.
    Se ha añadido la importación de `Domain.Repositories`.
    Se han eliminado los métodos `AddCoffeeToCart`, `GetCoffesInCart` y `RemoveCoffeeFromCart` de la clase `CartRepository`.
    Esto sugiere que la funcionalidad de gestión del carrito de compras ya no está implementada en esta clase.

--- src/Infrastructure/Api/CartRepository/CartRepository.cs
+++ src/Infrastructure/Api/CartRepository/CartRepository.cs
@@ -1,23 +1,8 @@
-﻿using Domain.Models;
-using Domain.Repositories;
+﻿using Domain.Repositories;
 
 namespace Infrastructure.Api.CartRepository
 {
     public class CartRepository : ICartRepository
     {
-        public Task AddCoffeeToCart(int coffeeId)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<List<Coffee>> GetCoffesInCart()
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task RemoveCoffeeFromCart(int coffeeId)
-        {
-            throw new NotImplementedException();
-        }
     }
 }
