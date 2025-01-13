e8846a863dfbe4c3f88519cd039f780dcfc3ff26
Eimar <eimar.ramossacari@gmail.com>
2025-01-13 23:22:31 +0100

    Refactorización de `ICartRepository` y ajustes de espacio de nombres
    Se ha eliminado la importación de `Domain.Models` porque ya no es necesaria.
    Se ha añadido una llave de apertura `{` para encapsular el contenido del espacio de nombres `Domain.Repositories`.
    Se han eliminado los métodos `GetCoffesInCart`, `AddCoffeeToCart` y `RemoveCoffeeFromCart` de la interfaz `ICartRepository`.
    Se ha añadido una llave de cierre `}` para cerrar el espacio de nombres `Domain.Repositories`.

--- src/Domain/Repositories/ICartRepository.cs
+++ src/Domain/Repositories/ICartRepository.cs
@@ -1,11 +1,6 @@
-﻿using Domain.Models;
-
-namespace Domain.Repositories
+﻿namespace Domain.Repositories
 {
     public interface ICartRepository
     {
-        Task<List<Coffee>> GetCoffesInCart();
-        Task AddCoffeeToCart(int coffeeId);
-        Task RemoveCoffeeFromCart(int coffeeId);
     }
 }
