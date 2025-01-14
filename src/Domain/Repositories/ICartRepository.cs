bda88a1bcb151bbec47b0790ddaae1200ea5560a
Eimar <eimar.ramossacari@gmail.com>
2025-01-14 23:13:04 +0100

    Añadir métodos para gestionar el carrito de café
    Se ha añadido una nueva línea para importar el espacio de nombres `Domain.Models`.
    Se ha añadido un nuevo método `GetCoffes` que devuelve una lista de `CartDetail`.
    Se ha añadido un nuevo método `AddCoffeeToCart` que toma un `coffeeId` como parámetro.
    Se ha añadido un nuevo método `RemoveCoffeeFromCart` que toma un `coffeeId` como parámetro.

--- src/Domain/Repositories/ICartRepository.cs
+++ src/Domain/Repositories/ICartRepository.cs
@@ -1,6 +1,11 @@
-﻿namespace Domain.Repositories
+﻿using Domain.Models;
+
+namespace Domain.Repositories
 {
     public interface ICartRepository
     {
+        Task<List<CartDetail>> GetCoffes();
+        Task AddCoffeeToCart(int coffeeId);
+        Task RemoveCoffeeFromCart(int coffeeId);
     }
 }
