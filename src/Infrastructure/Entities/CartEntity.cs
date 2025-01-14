c9f857fdbcaf30976f2a1ac5629465fb44ab4a48
Eimar <eimar.ramossacari@gmail.com>
2025-01-14 23:13:44 +0100

    Añadir clase CartEntity con propiedades y herencia
    Se ha introducido una nueva clase `CartEntity` en el archivo `CartEntity.cs` que hereda de `BaseEntity`. Esta clase incluye una propiedad `Total` de tipo `decimal` y una propiedad `CartDetails` de tipo `ICollection<CartDetailEntity>`, inicializada como una lista vacía. Además, se ha añadido el uso del espacio de nombres `Infrastructure.Common` y la clase se encuentra dentro del espacio de nombres `Infrastructure.Entities`.

---
+++ src/Infrastructure/Entities/CartEntity.cs
@@ -0,0 +1,10 @@
+﻿using Infrastructure.Common;
+
+namespace Infrastructure.Entities
+{
+    public class CartEntity : BaseEntity
+    {
+        public decimal Total { get; set; }
+        public ICollection<CartDetailEntity> CartDetails { get; set; } = [];
+    }
+}
