3d6395e3d6caf28bc07d2fa3aa1c37aabf93e0c7
Eimar <eimar.ramossacari@gmail.com>
2025-01-14 23:13:34 +0100

    Añadir clase CartDetailEntity en Infrastructure.Entities
    Se ha añadido la clase CartDetailEntity en el archivo CartDetailEntity.cs.
    Esta clase hereda de BaseEntity y contiene las propiedades CoffeeEntity? Coffee,
    CartEntity? Cart y int Quantity. También se ha añadido una directiva using para
    Infrastructure.Common.

---
+++ src/Infrastructure/Entities/CartDetailEntity.cs
@@ -0,0 +1,11 @@
+﻿using Infrastructure.Common;
+
+namespace Infrastructure.Entities
+{
+    public class CartDetailEntity : BaseEntity
+    {
+        public CoffeeEntity? Coffee { get; set; }
+        public CartEntity? Cart { get; set; }
+        public int Quantity { get; set; }
+    }
+}
