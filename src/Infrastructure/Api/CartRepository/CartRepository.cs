e7dc394e3629cf25d5c3aa3096a1efddb635a9c5
Eimar <eimar.ramossacari@gmail.com>
2025-01-14 23:13:15 +0100

    Añadir métodos y constructor a CartRepository
    Se han añadido varias declaraciones de `using` para importar los espacios de nombres necesarios: `AutoMapper`, `Domain.Models`, `Infrastructure.Entities`, `Infrastructure.Persistence` y `Microsoft.EntityFrameworkCore`.
    
    Se ha añadido un constructor a la clase `CartRepository` que inicializa los campos `_context` y `_mapper` con las instancias proporcionadas.
    
    Se ha añadido el método `GetCoffes` que obtiene una lista de detalles del carrito (`CartDetail`) desde la base de datos, mapeando las entidades de la base de datos a los modelos de dominio utilizando `AutoMapper`.
    
    Se han añadido los métodos `AddCoffeeToCart` y `RemoveCoffeeFromCart` que actualmente lanzan una excepción `NotImplementedException`.

--- src/Infrastructure/Api/CartRepository/CartRepository.cs
+++ src/Infrastructure/Api/CartRepository/CartRepository.cs
@@ -1,8 +1,38 @@
-﻿using Domain.Repositories;
+﻿using AutoMapper;
+using Domain.Models;
+using Domain.Repositories;
+using Infrastructure.Entities;
+using Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Api.CartRepository
 {
     public class CartRepository : ICartRepository
     {
+        private readonly DatabaseContext _context;
+        private readonly IMapper _mapper;
+        public CartRepository(DatabaseContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+        public async Task<List<CartDetail>> GetCoffes()
+        {
+            CartEntity? cart = await _context.Carts.AsNoTracking()
+                                                   .Include(c => c.CartDetails)
+                                                   .FirstAsync();
+
+            List<CartDetail> cartDetailsMapped = _mapper.Map<List<CartDetail>>(cart.CartDetails);
+
+            return cartDetailsMapped;
+        }
+        public async Task AddCoffeeToCart(int coffeeId)
+        {
+            throw new NotImplementedException();
+        }
+        public Task RemoveCoffeeFromCart(int coffeeId)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
