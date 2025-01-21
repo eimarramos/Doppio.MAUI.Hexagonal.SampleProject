ec08943f6a5f626ce837acc8c0f62f88e5049a67
Eimar <eimar.ramossacari@gmail.com>
2025-01-21 22:34:48 +0100

    Añadir métodos a ICartRepository para contar y totalizar
    Se han añadido dos nuevos métodos a la interfaz `ICartRepository`:
    - `Task<int> GetCurrentItemsCount();`: Devuelve la cantidad actual de artículos en el carrito.
    - `Task<decimal> GetTotal();`: Devuelve el total del carrito.
    
    Estos cambios amplían la funcionalidad de la interfaz para proporcionar más información sobre el estado del carrito de compras.

--- src/Domain/Repositories/ICartRepository.cs
+++ src/Domain/Repositories/ICartRepository.cs
@@ -5,6 +5,8 @@ namespace Domain.Repositories
     public interface ICartRepository
     {
         Task<List<CartDetail>> GetCoffes();
+        Task<int> GetCurrentItemsCount();
+        Task<decimal> GetTotal();
         Task AddCoffeeToCart(int coffeeId);
         Task RemoveCoffeeFromCart(int coffeeId);
     }
