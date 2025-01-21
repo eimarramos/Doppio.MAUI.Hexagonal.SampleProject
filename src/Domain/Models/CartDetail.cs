77b38a6957c3396886f64cd430a14159370e280b
Eimar <eimar.ramossacari@gmail.com>
2025-01-21 22:34:32 +0100

    Refactorización de propiedades en `CartDetail`
    Se han eliminado las propiedades `CoffeeId`, `CoffeeName`, `CoffeeDescription`, `CoffeeImageSource` y `CoffeePrice` de la clase `CartDetail`. En su lugar, se ha añadido una nueva propiedad `Coffee` de tipo `Coffee?` para encapsular toda la información del café. Esta refactorización mejora la estructura del código y facilita la gestión de la información del café dentro de la clase `CartDetail`.

--- src/Domain/Models/CartDetail.cs
+++ src/Domain/Models/CartDetail.cs
@@ -3,12 +3,7 @@
     public class CartDetail
     {
         public int Id { get; set; }
-        public int CoffeeId { get; set; }
-        public int CartId { get; set; }
-        public string CoffeeName { get; set; } = string.Empty;
-        public string CoffeeDescription { get; set; } = string.Empty;
-        public string CoffeeImageSource { get; set; } = string.Empty;
-        public decimal CoffeePrice { get; set; }
+        Coffee? Coffee { get; set; }
         public int Quantity { get; set; }
     }
 }
