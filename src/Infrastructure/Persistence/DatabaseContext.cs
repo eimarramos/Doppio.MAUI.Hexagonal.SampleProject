e3c01653bf40f7534676b85698f02c98e820dee0
Eimar <eimar.ramossacari@gmail.com>
2025-01-21 22:36:02 +0100

    Refactorización de DatabaseContext y configuración de modelos
    Se ha eliminado la importación de `Infrastructure.Config` y se ha añadido `System.Reflection`.
    El constructor de `DatabaseContext` ahora acepta `DbContextOptions<DatabaseContext>` y `IServiceProvider`.
    Se ha eliminado el método `OnConfiguring` que configuraba SQLite.
    Se ha añadido el método `OnModelCreating` que aplica configuraciones desde el ensamblado actual.

--- src/Infrastructure/Persistence/DatabaseContext.cs
+++ src/Infrastructure/Persistence/DatabaseContext.cs
@@ -1,20 +1,22 @@
-﻿using Infrastructure.Config;
-using Infrastructure.Entities;
+﻿using Infrastructure.Entities;
 using Microsoft.EntityFrameworkCore;
+using System.Reflection;
 
 namespace Infrastructure.Persistence
 {
     public class DatabaseContext : DbContext
     {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options, IServiceProvider serviceProvider)
+        : base(options){}
         public DbSet<CategoryEntity> Categories { get; set; } = null!;
         public DbSet<ShopEntity> Shops { get; set; } = null!;
         public DbSet<CoffeeEntity> Coffees { get; set; } = null!;
         public DbSet<CartEntity> Carts { get; set; } = null!;
         public DbSet<CartDetailEntity> CartDetails { get; set; } = null!;
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        protected override void OnModelCreating(ModelBuilder builder)
         {
-            string DbConnetion = $"Filename={Constants.DatabasePath}";
-            optionsBuilder.UseSqlite(DbConnetion);
+            base.OnModelCreating(builder);
+            builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
         }
     }
 }
