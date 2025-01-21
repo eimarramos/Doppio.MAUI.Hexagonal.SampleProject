a56fabd47514dd9ee0126c709a180fc087fa9ea3
Eimar <eimar.ramossacari@gmail.com>
2025-01-21 22:36:23 +0100

    Añadir repositorios, EF Core y configuración SQLite
    Se ha añadido la importación de varios repositorios y configuraciones de la infraestructura, así como de Entity Framework Core.
    Se ha definido una cadena de conexión a la base de datos SQLite utilizando una constante para la ruta de la base de datos.
    Se ha añadido un interceptor `CartTotalInterceptor` para manejar cambios en la base de datos.
    Se ha modificado la configuración del contexto de la base de datos para incluir los interceptores y utilizar SQLite.
    Se ha añadido el repositorio `CartRepository` al contenedor de servicios.
    Se ha reorganizado el código para asegurar que la inicialización de la base de datos y la configuración de AutoMapper se realicen correctamente.
    Se ha eliminado el código redundante que inicializaba y aseguraba la creación de la base de datos directamente.

--- src/Infrastructure/DependencyInjection.cs
+++ src/Infrastructure/DependencyInjection.cs
@@ -1,9 +1,12 @@
 ﻿using Domain.Repositories;
+using Infrastructure.Api.CartRepository;
 using Infrastructure.Api.CategoryRepository;
 using Infrastructure.Api.CoffeeRepository;
 using Infrastructure.Api.ShopRepository;
+using Infrastructure.Config;
 using Infrastructure.Persistence;
 using Infrastructure.Persistence.Interceptors;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Diagnostics;
 using Microsoft.Extensions.DependencyInjection;
 using System.Reflection;
@@ -14,35 +17,32 @@ namespace Infrastructure
     {
         public static IServiceCollection AddInfrastructureServices(this IServiceCollection services)
         {
-            //AutoMapper
+            string DbConnetion = $"Filename={Constants.DatabasePath}";
 
-            services.AddAutoMapper(Assembly.GetExecutingAssembly());
-
-            // Database
-
-            services.AddDbContext<DatabaseContext>();
-            var dbContext = new DatabaseContext();
-            dbContext.Database.EnsureCreated();
-            dbContext.Dispose();
-
-            // Database Initializer
-
-            services.AddSingleton<DatabaseContextInitializer>();
-
-            // Repositories
-
-            services.AddScoped<ICategoryRepository, CategoryRepository>();
-            services.AddScoped<IShopRepository, ShopRepository>();
-            services.AddScoped<ICoffeeRepository, CoffeeRepository>();
-
-            // Interceptors
+            // Database and interceptors
 
             services.AddScoped<ISaveChangesInterceptor, CartTotalInterceptor>();
 
             services.AddDbContext<DatabaseContext>((sp, options) =>
             {
                 options.AddInterceptors(sp.GetServices<ISaveChangesInterceptor>());
+                options.UseSqlite(DbConnetion);
             });
+
+            // AutoMapper
+
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+            // Repositories
+
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
+            services.AddScoped<IShopRepository, ShopRepository>();
+            services.AddScoped<ICoffeeRepository, CoffeeRepository>();
+            services.AddScoped<ICartRepository, CartRepository>();
+
+            // Database Initializer
+
+            services.AddSingleton<DatabaseContextInitializer>();
 
             return services;
         }
