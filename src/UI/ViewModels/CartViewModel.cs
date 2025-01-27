91a4a7765b9dd32491fbc5db40f37bb83178b42e
Eimar Ramos <eimar.ramossacari@gmail.com>
2025-01-27 23:28:36 +0100

    Refactorización de CartViewModel y nuevos comandos
    Se han añadido varias importaciones nuevas: `System.Collections.ObjectModel`, `ApplicationLayer.Services.CartService`, `CommunityToolkit.Mvvm.ComponentModel`, `CommunityToolkit.Mvvm.Input` y `Domain.Models`.
    
    Se ha eliminado la declaración del constructor vacío `public CartViewModel()`.
    
    Se ha añadido un campo privado `_cartService` de tipo `CartService`.
    
    Se ha añadido una propiedad observable `CartDetails` de tipo `ObservableCollection<CartDetail>`.
    
    Se ha modificado el constructor `CartViewModel` para aceptar un parámetro de tipo `CartService` y asignarlo al campo `_cartService`.
    
    Se ha añadido un comando `GetCartDetails` que obtiene los detalles del carrito de compras de `_cartService` y los asigna a la propiedad `CartDetails`. Este comando también maneja el estado de carga (`IsBusy`) y captura cualquier excepción que ocurra.
    
    Se ha añadido un comando `Disappearing` que actualmente no tiene ninguna implementación.

--- src/UI/ViewModels/CartViewModel.cs
+++ src/UI/ViewModels/CartViewModel.cs
@@ -1,12 +1,49 @@
-﻿using UI.ViewModels.SharedViewModels;
+﻿using System.Collections.ObjectModel;
+using ApplicationLayer.Services.CartService;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Domain.Models;
+using UI.ViewModels.SharedViewModels;
 
 namespace UI.ViewModels
 {
     public partial class CartViewModel : BaseViewModel
     {
-        public CartViewModel()
+        private readonly CartService _cartService;
+
+        [ObservableProperty]
+        private ObservableCollection<CartDetail> _cartDetails = [];
+
+        public CartViewModel(CartService cartService)
         {
             Title = "Cart";
+            _cartService = cartService;
+        }
+
+        [RelayCommand]
+        private async Task GetCartDetails()
+        {
+            try
+            {
+                IsBusy = true;
+
+                var cartDetails = await _cartService.GetCartDetails();
+                CartDetails = new ObservableCollection<CartDetail>(cartDetails);
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine(ex);
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+        }
+
+        [RelayCommand]
+        private void Disappearing()
+        {
+
         }
     }
 }
