8648a41e91e4c0adc3f3494a87a4d8619a65d1b4
Eimar Ramos <eimar.ramossacari@gmail.com>
2025-01-30 23:40:38 +0100

    Hacer pública la propiedad Coffee en CartDetail
    Se ha cambiado la propiedad `Coffee` de la clase `CartDetail` de tener un modificador de acceso predeterminado (interno) a ser pública.

--- src/Domain/Models/CartDetail.cs
+++ src/Domain/Models/CartDetail.cs
@@ -3,7 +3,7 @@
     public class CartDetail
     {
         public int Id { get; set; }
-        Coffee? Coffee { get; set; }
+        public Coffee? Coffee { get; set; }
         public int Quantity { get; set; }
     }
 }
