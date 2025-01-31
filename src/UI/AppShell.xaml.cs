07f7803d783bf376b6352ccef035f7cfbd0b7580
Eimar Ramos <eimar.ramossacari@gmail.com>
2025-01-31 01:27:35 +0100

    Añadir nuevas rutas y actualizar configuración de compilación
    Se han añadido varias rutas nuevas en `AppShell.xaml.cs` para las páginas `LoginPage`, `PromotionsPage`, `CartPage`, `HomePage`, `NotificationsPage` y `ProfilePage`, permitiendo la navegación a estas páginas dentro de la aplicación.
    
    Se han importado nuevos espacios de nombres en `AppShell.xaml.cs` para las vistas correspondientes a las nuevas rutas añadidas.
    
    En `UI.csproj`, se ha actualizado la configuración para compilar nuevas vistas XAML, específicamente `CartDetailView.xaml` dentro de la carpeta `Views\Cart\ContentViews`.

--- src/UI/AppShell.xaml.cs
+++ src/UI/AppShell.xaml.cs
@@ -1,4 +1,10 @@
-﻿using UI.Views.Menu;
+﻿using UI.Views.Cart;
+using UI.Views.Home;
+using UI.Views.Login;
+using UI.Views.Menu;
+using UI.Views.Notifications;
+using UI.Views.Profile;
+using UI.Views.Promotions;
 using UI.Views.ShopDetails;
 
 namespace UI
@@ -8,8 +14,14 @@ namespace UI
         public AppShell()
         {
             InitializeComponent();
+            Routing.RegisterRoute("login", typeof(LoginPage));
             Routing.RegisterRoute("shop_details", typeof(ShopDetailsPage));
             Routing.RegisterRoute("menu", typeof(MenuPage));
+            Routing.RegisterRoute("promotions", typeof(PromotionsPage));
+            Routing.RegisterRoute("cart", typeof(CartPage));
+            Routing.RegisterRoute("home", typeof(HomePage));
+            Routing.RegisterRoute("notifications", typeof(NotificationsPage));
+            Routing.RegisterRoute("profile", typeof(ProfilePage));
         }
     }
 }
