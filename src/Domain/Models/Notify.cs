f5e66c061e99d830de2bc9d2e158e92abeb43e4c
Eimar Ramos <eimar.ramossacari@gmail.com>
2025-02-11 22:46:31 +0100

    Añadir clase Notify en Domain.Models
    Se ha añadido un nuevo archivo `Notify.cs` que define el espacio de nombres `Domain.Models`. Dentro de este espacio de nombres, se ha creado una nueva clase pública llamada `Notify` con tres propiedades: `Id` de tipo `int`, `Coffee` de tipo `Coffee?` y `Progress` de tipo `int`. Esta clase está diseñada para notificar sobre el progreso relacionado con un objeto `Coffee`.

---
+++ src/Domain/Models/Notify.cs
@@ -0,0 +1,9 @@
+﻿namespace Domain.Models
+{
+    public class Notify
+    {
+        public int Id { get; set; }
+        public Coffee? Coffee { get; set; }
+        public int Progress { get; set; }
+    }
+}
