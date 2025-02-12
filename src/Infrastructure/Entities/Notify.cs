479f3995f9d7a17735897088d5cb79d68806b3d2
Eimar Ramos <eimar.ramossacari@gmail.com>
2025-02-12 22:44:08 +0100

    Añadir clase Notify en Infrastructure.Entities
    Se ha introducido una nueva clase llamada `Notify` en el archivo `Notify.cs`.
    Esta clase pertenece al espacio de nombres `Infrastructure.Entities` y hereda de `BaseEntity`.
    La clase `Notify` incluye dos propiedades: una propiedad `Coffee` de tipo `CoffeeEntity?` que puede ser nula, y una propiedad `Progress` de tipo `int`.
    Además, se ha añadido una directiva `using` para `Infrastructure.Common`.

---
+++ src/Infrastructure/Entities/Notify.cs
@@ -0,0 +1,10 @@
+﻿using Infrastructure.Common;
+
+namespace Infrastructure.Entities
+{
+    public class Notify : BaseEntity
+    {
+        public CoffeeEntity? Coffee { get; set; }
+        public int Progress { get; set; }
+    }
+}
