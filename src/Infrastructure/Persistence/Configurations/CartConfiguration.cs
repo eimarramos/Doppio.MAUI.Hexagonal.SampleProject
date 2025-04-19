c22a645275fde61a129ccb9d30ce872bc62a836f
Eimar Ramos <eimar.ramossacari@gmail.com>
2025-04-19 05:50:34 +0200

    Configurar entidad CartEntity en CartConfiguration.cs
    Se agregó la clase `CartConfiguration` en el espacio de nombres
    `Infrastructure.Persistence.Configurations`, implementando la
    interfaz `IEntityTypeConfiguration<CartEntity>`. En el método
    `Configure`, se definió la clave primaria de `CartEntity` con
    `HasKey(Id)`. También se importaron los espacios de nombres
    necesarios para la configuración.

---
+++ src/Infrastructure/Persistence/Configurations/CartConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Infrastructure.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Persistence.Configurations
+{
+    public class CartConfiguration : IEntityTypeConfiguration<CartEntity>
+    {
+        public void Configure(EntityTypeBuilder<CartEntity> builder)
+        {
+            builder.HasKey(c => c.Id );
+        }
+    }
+}
