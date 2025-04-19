4188e6885612f275832f94cfde24a89922fa0fc9
Eimar Ramos <eimar.ramossacari@gmail.com>
2025-04-19 21:00:48 +0200

    Añadidas clases Promotion y PromotionEntity
    Se añadió la clase `Promotion` en `Domain.Models` para representar una promoción con propiedades como `Id`, `Title`, `Shop`, `Coffee` y `Expiration`.
    
    Se añadió la clase `PromotionEntity` en `Infrastructure.Entities`, heredando de `BaseEntity`, para manejar promociones en la capa de infraestructura con propiedades similares.

---
+++ src/Domain/Models/Promotion.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain.Models
+{
+    public class Promotion
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public Shop? Shop { get; set; }
+        public Coffee? Coffee { get; set; }
+        public DateTimeOffset Expiration { get; set; }
+    }
+}
---
+++ src/Infrastructure/Entities/PromotionEntity.cs
@@ -0,0 +1,12 @@
+﻿using Infrastructure.Common;
+
+namespace Infrastructure.Entities
+{
+    public class PromotionEntity : BaseEntity
+    {
+        public string Title { get; set; } = string.Empty;
+        public ShopEntity? Shop { get; set; }
+        public CoffeeEntity? Coffee { get; set; }
+        public DateTimeOffset Expiration { get; set; }
+    }
+}
