44ce6974c84f75ad54b83847d09bee03ad7bcec9
Eimar Ramos <eimar.ramossacari@gmail.com>
2025-04-19 21:16:13 +0200

    Implementación de PromotionRepository con método GetAll
    Se agregó la clase `PromotionRepository` en `Infrastructure.Api.PromotionRepository`, implementando la interfaz `IPromotionRepository`.
    La clase utiliza `DatabaseContext` y `IMapper` como dependencias inyectadas.
    
    Se añadió el método asíncrono `GetAll`, que recibe un `shopId` y devuelve una lista de objetos `Promotion`.
    El método usa Entity Framework Core para consultar la base de datos, incluyendo relaciones con `Shop` y `Coffee`, y mapea las entidades a objetos del dominio con AutoMapper.
    
    Se añadieron los `using` necesarios para los espacios de nombres requeridos.

---
+++ src/Infrastructure/Api/PromotionRepository/PromotionRepository.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using Domain.Models;
+using Domain.Repositories;
+using Infrastructure.Entities;
+using Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Api.PromotionRepository
+{
+    public class PromotionRepository : IPromotionRepository
+    {
+        private readonly DatabaseContext _context;
+        private readonly IMapper _mapper;
+        public PromotionRepository(DatabaseContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public async Task<List<Promotion>> GetAll(int shopId)
+        {
+            List<PromotionEntity> promotions = await _context.Promotions
+                                                       .AsNoTracking()
+                                                       .Include(p => p.Shop)
+                                                       .Include(p => p.Coffee)
+                                                       .ToListAsync();
+
+            List<Promotion> promotionsMapped = _mapper.Map<List<Promotion>>(promotions);
+
+            return promotionsMapped;
+        }
+    }
+}
