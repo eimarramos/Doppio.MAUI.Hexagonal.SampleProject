821c1ad767d3644e24582ea1c9b9b57e2d4bf3a3
Eimar Ramos <eimar.ramossacari@gmail.com>
2025-04-21 15:22:44 +0200

    Refactor: mejoras en PromotionsViewModel
    - Añadidas dependencias necesarias mediante declaraciones `using`.
    - Reemplazado constructor vacío por uno con inyección de `PromotionService`.
    - Añadido campo privado `_promotionsService` para gestionar el servicio.
    - Implementada propiedad observable `_promotions` con `ObservableCollection`.
    - Añadido método `LoadDataAsync()` para cargar datos de forma asíncrona.
    - Añadido método privado `GetPromotionsAsync()` para obtener promociones.
    - Mejorada arquitectura con inyección de dependencias y asincronía.

--- src/UI/ViewModels/PromotionsViewModel.cs
+++ src/UI/ViewModels/PromotionsViewModel.cs
@@ -1,12 +1,50 @@
-﻿using UI.ViewModels.SharedViewModels;
+﻿using System.Collections.ObjectModel;
+using ApplicationLayer.Services.PromotionService;
+using CommunityToolkit.Mvvm.ComponentModel;
+using Domain.Models;
+using UI.ViewModels.SharedViewModels;
 
 namespace UI.ViewModels
 {
     public partial class PromotionsViewModel : BaseViewModel
     {
-        public PromotionsViewModel()
-        { 
+        private readonly PromotionService _promotionsService;
+
+        [ObservableProperty]
+        private ObservableCollection<Promotion> _promotions = new ObservableCollection<Promotion>();
+
+        public PromotionsViewModel(PromotionService promotionService)
+        {
             Title = "Promotions";
+
+            _promotionsService = promotionService;
+
+            LoadDataAsync();
+        }
+
+        public async void LoadDataAsync()
+        {
+            try
+            {
+                IsBusy = true;
+
+                Promotions = await GetPromotionsAsync();
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine(ex);
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+        }
+
+        private async Task<ObservableCollection<Promotion>> GetPromotionsAsync()
+        {
+            var shops = await _promotionsService.GetAll();
+
+            return new ObservableCollection<Promotion>(shops);
         }
     }
 }
