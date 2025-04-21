5d7f2dcdcbe3e71e6f3079bc86fd48303af1639f
Eimar Ramos <eimar.ramossacari@gmail.com>
2025-04-21 15:21:33 +0200

    Añadir clase PromotionService con método GetAll
    - Se creó el espacio de nombres `ApplicationLayer.Services.PromotionService`.
    - Se añadió la clase `PromotionService` con un campo privado `_promotionRepository` de tipo `IPromotionRepository`, inicializado en el constructor.
    - Se implementó el método público y asíncrono `GetAll` para obtener y devolver una lista de promociones (`List<Promotion>`).
    - Se añadieron las declaraciones `using Domain.Models;` y `using Domain.Repositories;` para importar los espacios de nombres necesarios.

---
+++ src/ApplicationLayer/Services/PromotionService/PromotionService.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Models;
+using Domain.Repositories;
+
+namespace ApplicationLayer.Services.PromotionService
+{
+    public class PromotionService
+    {
+        private readonly IPromotionRepository _promotionRepository;
+
+        public PromotionService(IPromotionRepository promotionRepository)
+        {
+            _promotionRepository = promotionRepository;
+        }
+
+        public async Task<List<Promotion>> GetAll()
+        {
+            return await _promotionRepository.GetAll();
+        }
+    }
+}
